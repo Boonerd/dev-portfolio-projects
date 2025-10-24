6dcc2fe32905613ab8d51a31e1622139e22eeb1e
PattyMbua <patriciah.mbua@starthmore.edu>
2025-10-24 18:55:39 +0300

    Fix base path and rebuild for empty page

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Remove base for local dev, add for build
+  base: process.env.NODE_ENV === 'production' ? '/dev-portfolio-projects/' : '/',
   plugins: [react()],
 });
