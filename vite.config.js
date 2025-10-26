d46bd7d9612bee7f06ace0df2422bd02b2b9c046
PattyMbua <patriciah.mbua@starthmore.edu>
2025-10-26 11:10:15 +0300

    Reorganized project folder, preparing for redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dev-portfolio-projects/',
   plugins: [react()],
-});
+  base: '/dev-portfolio-projects/', 
+})
